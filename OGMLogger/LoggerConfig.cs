f84d95e7313042363e7eafa48c558be69fef0be2
HonkerForce <scorpiowolf@qq.com>
2024-07-03 17:42:40 +0800

    调整.net标准

---
+++ OGMLogger/LoggerConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace OGMUtility.Logger
+{
+    public class LoggerConfig
+    {
+        public enum LoggerType
+        {
+            UnityClient,
+            NetServer,
+        }
+        public bool logActive = true;
+        public string logPrefix = "@";
+        public bool showTime = true;
+        public string logSep = ">>";
+        public bool showTread = true;
+        public bool isSave = true;
+        public bool coverSave = true;
+        public string savePath = $"{AppDomain.CurrentDomain.BaseDirectory}Logs\\";
+        public string saveFileName = "GameLog.txt";
+        public LoggerType loggerType = LoggerType.NetServer;
+    }
+}
